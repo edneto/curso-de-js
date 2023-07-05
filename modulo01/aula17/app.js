9fc1a4e7160b1c9b5939d5ae4f5900f29bf6b44f
Luiz Lima <edneto.eternamente@gmail.com>
2023-07-05 20:42:40 -0300

    update class 19

---
+++ modulo01/aula17/app.js
@@ -0,0 +1,8 @@
+//Undefined
+//let emptiness
+// console.log(emptiness, emptiness + 3, `o valor é ${emptiness}`)
+
+
+//Null
+//let emptiness = null
+// console.log(emptiness, emptiness + 3, `o valor é ${emptiness}`)
